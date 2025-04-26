a0d5d5df288fb0bbc3a939fc3109172160004532
kiyongee2 <kiyongee2@gmail.com>
2025-04-26 15:23:16 +0900

    singleton, String method

--- Chapter4/src/classes/BookTest.java
+++ Chapter4/src/classes/BookTest.java
@@ -4,7 +4,7 @@ public class BookTest {
 
 	public static void main(String[] args) {
 		// 객체 배열 생성
-		/*Book[] books = new Book[3];
+		Book[] books = new Book[3];
 		
 		for(int i = 0; i < books.length; i++) {
 			System.out.println(books[i]);
@@ -13,14 +13,14 @@ public class BookTest {
 		//원소(요소) 추가(저장)
 		books[0] = new Book(100, "채식주의자", "한강");
 		books[1] = new Book(101, "자바 프로그래밍 입문", "박은종");
-		books[2] = new Book(102, "모두의 파이썬", "이승찬");*/
+		books[2] = new Book(102, "모두의 파이썬", "이승찬");
 		
 		//객체 배열 생성 방법2
-		Book[] books = {
+		/*Book[] books = {
 			new Book(100, "채식주의자", "한강"),	
 			new Book(101, "자바 프로그래밍 입문", "박은종"),
 			new Book(102, "모두의 파이썬", "이승찬")
-		};
+		};*/
 		
 		//특정 원소 출력
 		//books[0].showBookInfo();
